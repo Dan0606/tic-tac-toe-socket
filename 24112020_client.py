bda6cb45765bf4dcfb8d3e1dd1dc74af2fef8acc
Dan0606 <65502527+Dan0606@users.noreply.github.com>
2020-11-26 12:28:51 +0200

    only send and get row and column func in the game loop

---
+++ 24112020_client.py
@@ -0,0 +1,70 @@
+import socket
+
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 52472        # The port used by the server
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def print_board(board):
+    final_board = ""
+    counter = 1
+    for sign in board:
+        if counter == 4 or counter == 7:
+            final_board += "\n"
+        if sign == "0":
+            final_board += "_"
+            counter += 1
+        elif sign == "1":
+            final_board += "X"
+            counter += 1
+        elif sign == "2":
+            final_board += "O"
+            counter += 1
+    return final_board   
+
+def send_row_col():
+    play_or_wait = S.recv(1024).decode()
+    if play_or_wait == "PLAY":
+        print("your turn")
+        row = int(input("row - "))
+        col = int(input("col - "))
+        while row not in range(1,4) or col not in range(1,4):
+            print("unvalid row or column")
+            row = int(input("row - "))
+            col = int(input("col - "))
+        S.send(str(row).encode())
+        S.send(str(col).encode())
+            
+    elif play_or_wait == "WAIT":
+        print("wait for your turn")
+
+def game_loop_client():
+    while True:
+        game_play = S.recv(1024).decode()
+        if game_play == "PLAY":
+            S.send("PLAYING".encode())
+            send_row_col()
+        elif game_play == "DONT PLAY":
+            break
+
+
+def main():
+    S.connect((HOST, PORT))
+    name = input("enter your name: ")
+    S.send(name.encode()) # send hello after converting it to bytes
+    start_or_wait = S.recv(1024).decode()
+    if start_or_wait == "WAIT":
+        print("waiting for second player")
+        start_or_wait = S.recv(1024).decode()
+    if start_or_wait == "START":
+        print("starting the game")
+        game_loop_client()
+    
+
+    
+
+    #s.close()
+
+main()
+
---
+++ 24112020_server.py
@@ -0,0 +1,85 @@
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 52472        # Port to listen on (non-privileged ports are > 1023)
+CLIENTS = []
+BOARD = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+PLAYER_PLAYING = 0
+
+def send_all(msg):
+    for client in CLIENTS:
+        client.send(msg.encode())
+
+def convert_board_to_string():
+    msg = "BOARD"
+    for i in range(3):
+        for j in range(3):
+            msg += str(BOARD[i][j])
+    return msg
+
+def get_row_col():
+    global PLAYER_PLAYING
+    for client in CLIENTS:
+        if CLIENTS[PLAYER_PLAYING] == client:
+            client.send("PLAY".encode())
+        elif CLIENTS[PLAYER_PLAYING-1] == client or CLIENTS[PLAYER_PLAYING+1] == client:
+            client.send("WAIT".encode())
+    row = CLIENTS[PLAYER_PLAYING].recv(1024).decode()
+    col = CLIENTS[PLAYER_PLAYING].recv(1024).decode()
+    print(row, col)
+    if PLAYER_PLAYING == 0:
+        PLAYER_PLAYING = 1
+    elif PLAYER_PLAYING == 1:
+        PLAYER_PLAYING = 0
+    
+    
+
+def update_board(row_col):
+    row = row_col[0]
+    col = row_col[1]
+    if PLAYER_PLAYING == 0:
+        sign = 1
+    else:
+        sign = 2
+    BOARD[row-1][col-1] = sign
+        
+
+
+def game_loop_server():
+    game_play = True
+    while game_play:
+        send_all("PLAY")
+        for client in CLIENTS:
+            client.recv(1024).decode()          
+        get_row_col()
+    send_all("DONT PLAY")
+
+
+   
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create TCP socket
+    s.bind((HOST, PORT)) # bind the socket with the IP and the port
+    s.listen() # open the socket for client connections
+    print("waiting for clients...")
+    client_connection1, addr = s.accept() # wait until client will connect
+    CLIENTS.append(client_connection1)
+    name1 = client_connection1.recv(1024).decode()
+    print(f"{name1} is connected")
+    client_connection1.send("WAIT".encode())
+    client_connection2, addr = s.accept()  # wait until client will connect
+    CLIENTS.append(client_connection2)
+    name2 = client_connection2.recv(1024).decode()
+    print(f"{name2} is connected")
+    send_all("START")
+    print("STRATING GAME")
+    game_loop_server()
+
+    
+    
+
+
+
+    #s.close()
+
+
+main()
