f32ad0c63c8f3dc4830abbb22ad3fb7252e63a3b
Dan0606 <65502527+Dan0606@users.noreply.github.com>
2020-11-29 13:25:54 +0200

    Game Ready No Graphics

--- 24112020_client.py
+++ 24112020_client.py
@@ -1,7 +1,7 @@
 import socket
 
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '10.100.102.28'  # The server's hostname or IP address
 PORT = 52472        # The port used by the server
 S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -27,25 +27,42 @@ def send_row_col():
     play_or_wait = S.recv(1024).decode()
     if play_or_wait == "PLAY":
         print("your turn")
-        row = int(input("row - "))
-        col = int(input("col - "))
+        row = input("row - ")
+        col = input("col - ") 
+        while type(row) != int and type(col) != int:
+            try: 
+                row = int(row)
+                col = int(col)
+            except:
+                print("row and column must be numbers between 1-3")
+                row = input("row - ")
+                col = input("col - ")               
         while row not in range(1,4) or col not in range(1,4):
-            print("unvalid row or column")
+            print("row and column has to be between 1 to 3")
             row = int(input("row - "))
             col = int(input("col - "))
         S.send(str(row).encode())
         S.send(str(col).encode())
-            
+        return "this player is playing"
     elif play_or_wait == "WAIT":
         print("wait for your turn")
+        return "this player is not playing"
 
 def game_loop_client():
     while True:
         game_play = S.recv(1024).decode()
         if game_play == "PLAY":
             S.send("PLAYING".encode())
-            send_row_col()
+            player_playing = send_row_col()
+            if  player_playing == "this player is playing":
+                is_placed = S.recv(1024).decode()
+                while is_placed == "PLACE FULL":
+                    print("place already full, try another place")
+                    send_row_col()
+                    is_placed = S.recv(1024).decode()   
             board = S.recv(1024).decode()
+            while board == "WAIT":
+                board = S.recv(1024).decode()
             S.send("BOARD ARRIVED".encode())
             print(print_board(board))
         elif game_play == "GAME OVER":
@@ -64,11 +81,7 @@ def main():
     if start_or_wait == "START":
         print("starting the game")
         game_loop_client()
-    
-
-    
-
-    #s.close()
+    S.close()
 
 main()
 
--- 24112020_server.py
+++ 24112020_server.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '10.100.102.28'  # Standard loopback interface address (localhost)
 PORT = 52472        # Port to listen on (non-privileged ports are > 1023)
 CLIENTS = []
 BOARD = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -35,6 +35,17 @@ def is_game_over(): # return True if game is over and False if not
     if empty_places_counter == 0:
         return True
 
+    list_board = board_into_reg_list()
+    if (list_board[0] == list_board[3] and list_board[3] == list_board[6] and list_board[6] != 0) or (list_board[1] == list_board[4] and list_board[4] == list_board[7] and list_board[7] != 0) or (list_board[2] == list_board[5] and list_board[5] == list_board[8] and list_board[8] != 0) or (list_board[0] == list_board[4] and list_board[4] == list_board[8] and list_board[8] != 0) or (list_board[2] == list_board[4] and list_board[4] == list_board[6] and list_board[6] != 0):
+        return True
+
+def board_into_reg_list():
+    new_board = []
+    for i in BOARD:
+        for j in i:
+            new_board.append(j)
+    return new_board
+
 def get_row_col():
     global PLAYER_PLAYING
     for client in CLIENTS:
@@ -44,13 +55,20 @@ def get_row_col():
             client.send("WAIT".encode())
     row = CLIENTS[PLAYER_PLAYING].recv(1024).decode()
     col = CLIENTS[PLAYER_PLAYING].recv(1024).decode()
+
+    return row,col
+    
+def change_turn():
+    global PLAYER_PLAYING
     if PLAYER_PLAYING == 0:
         PLAYER_PLAYING = 1
     elif PLAYER_PLAYING == 1:
         PLAYER_PLAYING = 0
-    return row,col
-    
-    
+
+def check_place(row, col):
+    if BOARD[row-1][col-1] == 0:
+        return "empty"
+    return "not empty"
 
 def update_board(row, col):
     if PLAYER_PLAYING == 0:
@@ -58,11 +76,11 @@ def update_board(row, col):
     else:
         sign = 2
     BOARD[row-1][col-1] = sign
-        
 
 
 def game_loop_server():
     game_play = True
+    board_ready_to_send = False
     while game_play:
         if is_game_over():
             send_all("GAME OVER")
@@ -73,11 +91,23 @@ def game_loop_server():
         row_col = get_row_col()
         row = int(row_col[0])
         col = int(row_col[1])
-        update_board(row, col)
-        str_board = convert_board_to_string()
-        send_all(str_board)
+        is_empty = check_place(row, col)        
+        while is_empty == "not empty":
+            CLIENTS[PLAYER_PLAYING].send("PLACE FULL".encode())
+            row_col = get_row_col()
+            row = int(row_col[0])
+            col = int(row_col[1])
+            is_empty = check_place(row, col)
+        if is_empty == "empty":
+            update_board(row, col)
+            CLIENTS[PLAYER_PLAYING].send("PLACED".encode()) 
+            board_ready_to_send = True
+        if board_ready_to_send:        
+            str_board = convert_board_to_string()
+            send_all(str_board)
         for client in CLIENTS:
             client.recv(1024).decode()
+        change_turn()
 
 
 
@@ -99,13 +129,7 @@ def main():
     send_all("START")
     print("STRATING GAME")
     game_loop_server()
-
-    
-    
-
-
-
-    #s.close()
+    s.close()
 
 
 main()
